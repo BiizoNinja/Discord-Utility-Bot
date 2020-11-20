cdc3a0718efe8c5056feb754af2fce3eaae20991
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-20 19:47:48 +0530

    j

--- Commands/help.js
+++ Commands/help.js
@@ -9,9 +9,9 @@ module.exports = {
         .setTitle('Help Command!')
         .setDescription('Prefix is ">"')
         .addFields(
-            {name: '**Fun Section**', value: '`ping` , `boop` , `wel`'} ,
-            {name: '**Moderation Section**', value: '`Clear` , `Ban` , `Kick`'},
-            {name: '**Utility Section**', value: '`socials`'}
+            {name: '**Fun Section**', value: '`ping` , `boop` , `wel , `pet`'} ,
+            {name: '**Moderation Section**', value: '`Clear` , `Ban` , `Kick`', incline: true},
+            {name: '**Utility Section**', value: '`socials`, `deadchat`'}
         )
         .setColor('#3399ff')
         .setFooter('DM BiizoNinja#3337 for more info!')
