721146c8def121d07cfd3e403170e8298549a80b
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-20 16:52:08 +0530

    d

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const { clearScreenDown } = require('readline');
  
 Client.commands = new Discord.Collection();
  
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+    const command = require(`./Commands/${file}`);
  
     Client.commands.set(command.name, command);
 }
