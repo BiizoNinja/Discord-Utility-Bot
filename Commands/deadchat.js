33eb9b3e0989697ee146af714323989b56537db2
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-20 19:18:21 +0530

    d

--- Commands/deadchat.js
+++ Commands/deadchat.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'deadchat' ,
     description:'Pings for dead chat',
     execute(message, args) {
+        if(!message.members.permissions.has('MANAGE_MESSAGES'))
+        return message.reply('This command is locked for staff only!')
+        
         message.channel.send('<@&763413779676397589> oh dead chat eh?')
     }
 }
