bfa0ce871934c8d1f3404c5faa701fbc02d7d894
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-21 21:22:39 +0530

    s

--- Commands/Vote.js
+++ Commands/Vote.js
@@ -11,7 +11,7 @@ module.exports = {
         .addFields(
            {name: 'VOTE LINK' , value: 'https://top.gg/servers/727018252437291040/vote'}
         )
-        .setImage('https://media.discordapp.net/attachments/753832377054396448/779717997429260308/BiizoNinjas_Lab.png?width=461&height=461')
+        .setThumbnail('https://media.discordapp.net/attachments/753832377054396448/779717997429260308/BiizoNinjas_Lab.png?width=461&height=461')
         .setFooter('We really do appreciate votes!')
         .setColor('#0099ff')
         .setTimestamp();
