4ea6c8bc5f80ca23065cf48db011716838d15643
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-21 22:51:34 +0530

    d

--- Commands/warn.js
+++ Commands/warn.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'socials' ,
-    description:'Displays BiizoNinja\'s Socials!',
+    name: 'warn' ,
+    description:'warns a user!',
     execute(message, args) {
         message.channel.send('**This Command is still in dev**. This will release very soon! Keep an eye on <#761882444835323914> to know when this releases!')
     }
