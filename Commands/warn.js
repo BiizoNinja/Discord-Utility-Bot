2cec1d978cf9185b983ef292f720366c41770db6
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-22 15:51:39 +0530

    d

--- Commands/warn.js
+++ Commands/warn.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message, args) {
         if(!message.member.roles.cache.has('727076911926476822')) return message.reply("<:WokePepe:748831596785827870> You do not have the permission to do this!")
 
-        const Reason = message.content.split(' ').slice(2).join(" ") 
+        const Reason = args[2]
        
         const user = message.mentions.users.first(); 
 
@@ -16,7 +16,7 @@ module.exports = {
         if(Member){
             const WarnEmbed = new Discord.MessageEmbed()
             .setTitle('MEMBER WARNED!')
-            .setDescription(` <:up_vote:748383108465688637> ${user.tag} Has been warned for the reason ${Reason}`)
+            .setDescription(` <:up_vote:748383108465688637> ${user.tag} Has been warned for the reason ${args[2]}`)
             .setFooter('We highly recommend not to do this again!')
             .setColor('#00ccff')
             .setThumbnail('https://media.discordapp.net/attachments/753832377054396448/779717997429260308/BiizoNinjas_Lab.png?width=461&height=461')
@@ -24,7 +24,7 @@ module.exports = {
 
         message.channel.send(WarnEmbed);
                     
-        } else if(!Reason) 
+        } else if(!args[2]) 
         return message.reply('<:down_vote:748383106444034050> Please provide a reason')
                                   
         }
