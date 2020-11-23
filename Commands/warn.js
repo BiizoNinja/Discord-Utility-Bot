86cdb66798f44c3f175d1bbae027d6edde166f18
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-23 09:30:11 +0530

    s

--- Commands/warn.js
+++ Commands/warn.js
@@ -21,17 +21,12 @@ module.exports = {
         if(Member){
             let WarnEmbed = new Discord.MessageEmbed()
             .setTitle('MEMBER WARNED!')
-            .setDescription(` <a:animated_check_2:780277155874603008> **${user.tag}** Has been warned for the reason ${args[2]} > ${args[20]}`)
+            .setDescription(` <a:animated_check_2:780277155874603008> **${user.tag}** Has been warned for the reason ${args[2]} `)
             .setFooter('We highly recommend not to do this again!')
             .setColor('#00ccff')
             .setTimestamp();
 
-            if(args[20]) {
-                if(member){
-                    message.channel.send(WarnEmbed)
-                }
-            }
-                    
+           
         } else if(!Reason) 
         return message.reply('<:down_vote:748383106444034050> Please provide a reason')
                                   
