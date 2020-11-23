ee0a07978af67998f362a1a79e186cd1c3920e07
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-23 11:04:19 +0530

    s

--- Commands/wel.js
+++ Commands/wel.js
@@ -3,7 +3,7 @@ module.exports = {
     aliases: 'welcome' ,
     description:'Welcomes people!',
     execute(message, args) {
-        message.channel.send('<a:welcome_red_1:775359667470991381> <a:welcome_red_2:775359667957530624>')
+        message.channel.send('<a:welcome_red_1:775359667470991381><a:welcome_red_2:775359667957530624>')
 
 
     }
