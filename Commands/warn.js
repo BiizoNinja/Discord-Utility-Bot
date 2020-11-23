a16e34466e94f802f71c97b693af5b1cd4b1f50e
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-23 09:18:19 +0530

    epik

--- Commands/warn.js
+++ Commands/warn.js
@@ -17,15 +17,14 @@ module.exports = {
         if(Member){
             const WarnEmbed = new Discord.MessageEmbed()
             .setTitle('MEMBER WARNED!')
-            .setDescription(` <:up_vote:748383108465688637> ${user.tag} Has been warned for the reason ${args[2]}`)
+            .setDescription(` <a:animated_check_2:780277155874603008> **${user.tag}** Has been warned for the reason ${args[2]}`)
             .setFooter('We highly recommend not to do this again!')
             .setColor('#00ccff')
-            .setThumbnail('https://media.discordapp.net/attachments/753832377054396448/779717997429260308/BiizoNinjas_Lab.png?width=461&height=461')
             .setTimestamp();
 
         message.channel.send(WarnEmbed);
                     
-        } else if(!args[2]) 
+        } else if(!Reason) 
         return message.reply('<:down_vote:748383106444034050> Please provide a reason')
                                   
         }
