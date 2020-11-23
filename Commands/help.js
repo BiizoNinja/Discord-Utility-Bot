71be2cafb21c007ab928cf20066c05bf5e19324d
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-23 09:45:49 +0530

    s

--- Commands/help.js
+++ Commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
         .setTitle('Help Command!')
         .setDescription('Prefix is ">"')
         .addFields(
-            {name: '**Fun Section**', value: '`ping` , `boop` , `wel` , `pet`'} ,
+            {name: '**Fun Section**', value: '`ping` , `boop` , `wel` , '} ,
             {name: '**Moderation Section**', value: '`Clear` , `Ban` , `Kick` `slowmode` `warn` ', incline: true},
             {name: '**Utility Section**', value: '`socials`, `deadchat` `vote`'}
         )
