25258e0310caba8ed1e26e499ef6d1632c894a82
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-30 09:26:12 +0530

    s

---
+++ Commands/say.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'say',
+    description: "says stuff",
+    execute(message, args) {
+        const content = args.slice(1).join(' ')
+
+
+        if(content) return 
+        message.channel.send('What should i say smh...')
+
+        if(content) {
+            message.channel.send(content)
+        }
+    }
+}
