3b8cb217a7ab1f2b5c0a1048481b2402da0f0351
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-30 09:58:27 +0530

    s

--- Commands/rickroll.js
+++ Commands/rickroll.js
@@ -12,7 +12,7 @@ module.exports = {
 
         .setColor('#FF0000')
         .setTitle(`RICK ROLL!`)
-        .setDescription(message.author.username  +   `RickRolled` +  ` ${user} ` )
+        .setDescription(message.author.username   +    `RickRolled` +  ` ${user} ` )
         .setImage('https://media1.tenor.com/images/4324d537dbc06f422b34ae131c7b3e14/tenor.gif?itemid=7755460')
         .setTimestamp()
 
