69034af4432c527278f380a337da28a41b7c14ac
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2020-11-30 08:31:59 +0530

    j

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ for(const file of commandFiles){
 
 Client.on('ready',  () => {
 
-  Client.user.setActivity('BiizoNinja AFK! Sleeping... ' , {
+  Client.user.setActivity('waht hello server why dead?' , {
     type: 'PLAYING'
   })
   Client.on('guildMemberAdd', message => {
@@ -142,5 +142,5 @@ Client.on('message', message => {
 });
 
 
-//Client.login(process.env.token)
-Client.login('NzczODk5MDIzNTI5OTM0ODY4.X6P7oQ.k2k3qPuVMM2oYEboD-MKPCxOHrs');
+Client.login(process.env.token)
+//Client.login('NzczODk5MDIzNTI5OTM0ODY4.X6P7oQ.k2k3qPuVMM2oYEboD-MKPCxOHrs');
