65c736859443e16fe1964be0b0e039827ace4430
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-16 10:02:58 +0530

    prettified all the files ✨

--- Commands/ban.js
+++ Commands/ban.js
@@ -1,16 +1,17 @@
+
 module.exports = {
     name: 'ban' ,
     description:'Bans a user!',
     execute(message, args) {
 
-        if(!message.member.permissions.has('BAN_MEMBERS'))
+        if(!message.member.hasPermission('BAN_MEMBERS'))
         return message.reply('You don\'t have the permissions')
     
        const user = message.mentions.users.first(); 
 
        if(!user) return message.channel.send(`Please specify a member to ban :x:`)
-       if(!user == message.author) return message.channel.send(`You can ban yourself! :x:`)
-       if(!user == message.guild.owner) return message.channel.send(`You Can't ban the owner can you? :x:`)
+       if(user == message.author) return message.channel.send(`You can ban yourself! :x:`)
+       if(user == message.guild.owner) return message.channel.send(`You Can't ban the owner can you? :x:`)
        
        if(user) {
            const member = message.guild.member(user);
@@ -23,7 +24,7 @@ module.exports = {
                //DM's the banned user
                member.send(`You were banned from **${message.guild.name}**.`)
            }).catch(err =>{
-               message.reply(`There was a problem banning that user! \n **POSSIBLE ERROR** \n\`\`\` => I do not have a permission. \`\`\` `);
+               message.reply(`There was a problem banning that user! \n\n **POSSIBLE ERROR** \n\`\`\` => I do not have a permission. \`\`\` `);
                console.log(err);
            });
         }
@@ -31,5 +32,4 @@ module.exports = {
 
    }
    
-}
-      
+} 
